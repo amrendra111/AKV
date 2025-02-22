d453cbd863b4ee9b3de12020fd4b04b454b1ea54
amrendra111 <amrendrakumar1816@gmail.com>
2025-02-22 14:42:22 +0530

    added

---
+++ solution/src/test/java/com/akv/solutions/AkvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akv.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
